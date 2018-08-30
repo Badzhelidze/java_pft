4774fe26b417e5c88be3c561f8fc7354b70477b4
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-08-30 11:07:39 +0300

    Рефакторинг класса GroupDeletionTests
    Замена методов на уже созданнные ранее
        gotoGroupPage();
        returnToGroupPage();
    + создание двух новых методов:
    selectGroup();
            deleteSelectedGroups();

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -4,53 +4,37 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 import java.util.concurrent.TimeUnit;
 import java.util.Date;
 import java.io.File;
+
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.*;
+
 import static org.openqa.selenium.OutputType.*;
 
-public class GroupDeletionTests {
-    ChromeDriver wd;
-    
-    @BeforeMethod
-    public void setUp() throws Exception {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\badzhelidze\\Desktop\\dev\\java_pft\\chromedriver.exe");
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
+public class GroupDeletionTests extends TestBase {
+
+
     @Test
     public void testGroupDeletion() {
-        wd.get("http://localhost/addressbook/group.php");
-        wd.findElement(By.name("user")).click();
-        wd.findElement(By.name("user")).clear();
-        wd.findElement(By.name("user")).sendKeys("admin");
-        wd.findElement(By.name("pass")).click();
-        wd.findElement(By.name("pass")).clear();
-        wd.findElement(By.name("pass")).sendKeys("secret");
-        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-        wd.findElement(By.linkText("groups")).click();
-        wd.findElement(By.name("selected[]")).click();
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+    private void deleteSelectedGroups() {
         wd.findElement(By.name("delete")).click();
-        wd.findElement(By.linkText("group page")).click();
     }
-    
-    @AfterMethod
-    public void tearDown() {
-        wd.quit();
+
+    private void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
     }
-    
-    public static boolean isAlertPresent(ChromeDriver wd) {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
+
+
 }
