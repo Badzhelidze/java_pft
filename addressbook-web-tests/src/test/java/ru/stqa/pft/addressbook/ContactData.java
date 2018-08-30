dfc7880dc9c6f3feadb814416cca1ca6be6befb3
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-08-30 13:50:23 +0300

    Выделение вспомогательных методов и вспомогательных классов для ContactCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,73 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String myname;
+    private final String mysurname;
+    private final String mylastname;
+    private final String mynick;
+    private final String mytitle;
+    private final String myhome;
+    private final String myaddress;
+    private final String mycompany;
+    private final String mymobile;
+    private final String mywork;
+    private final String myfax;
+
+    public ContactData(String myname, String mysurname, String mylastname, String mynick, String mytitle, String myhome, String myaddress, String mycompany, String mymobile, String mywork, String myfax) {
+        this.myname = myname;
+        this.mysurname = mysurname;
+        this.mylastname = mylastname;
+        this.mynick = mynick;
+        this.mytitle = mytitle;
+        this.myhome = myhome;
+        this.myaddress = myaddress;
+        this.mycompany = mycompany;
+        this.mymobile = mymobile;
+        this.mywork = mywork;
+        this.myfax = myfax;
+    }
+
+    public String getMyname() {
+        return myname;
+    }
+
+    public String getMysurname() {
+        return mysurname;
+    }
+
+    public String getMylastname() {
+        return mylastname;
+    }
+
+    public String getMynick() {
+        return mynick;
+    }
+
+    public String getMytitle() {
+        return mytitle;
+    }
+
+    public String getMyhome() {
+        return myhome;
+    }
+
+    public String getMyaddress() {
+        return myaddress;
+    }
+
+    public String getMycompany() {
+        return mycompany;
+    }
+
+    public String getMymobile() {
+        return mymobile;
+    }
+
+    public String getMywork() {
+        return mywork;
+    }
+
+    public String getMyfax() {
+        return myfax;
+    }
+}
