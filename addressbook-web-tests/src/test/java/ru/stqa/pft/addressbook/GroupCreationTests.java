851b2fbb85c388788afd8fd7c1f5f4fb7c37724d
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-08-30 09:02:44 +0300

    Смена geckodriver на chromedriver
    Не работал запуск на firefox

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
@@ -10,23 +11,22 @@ import java.util.Date;
 import java.io.File;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
 import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
-    FirefoxDriver wd;
+    ChromeDriver wd;
 
     @BeforeMethod
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver","C:\\Users\\badzhelidze\\Desktop\\dev\\java_pft\\geckodriver.exe");
-        wd = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\badzhelidze\\Desktop\\dev\\java_pft\\chromedriver.exe");
+        wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
     
     @Test
-    public void GroupCreationTests() {
-        wd.get("http://127.0.0.1/addressbook/");
+    public void testGroupCreation() {
+        wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys("admin");
@@ -54,7 +54,7 @@ public class GroupCreationTests {
         wd.quit();
     }
     
-    public static boolean isAlertPresent(FirefoxDriver wd) {
+    public static boolean isAlertPresent(ChromeDriver wd) {
         try {
             wd.switchTo().alert();
             return true;
