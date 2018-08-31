6fec7c4393d850870be0ea8e60f92c758707162b
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-08-31 08:46:37 +0300

    Заменяем наследование на дилегирование
    Заменяем наследование на дилегирование
    refactor->Replace inheritance with deligation (но ничего не выбираем)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -1,25 +1,21 @@
 package ru.stqa.pft.addressbook;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.util.concurrent.TimeUnit;
+public class TestBase {
 
-public class TestBase extends ApplicationManager {
 
+    protected final ApplicationManager app = new ApplicationManager();
 
     @BeforeMethod
     public void setUp() throws Exception {
-        init();
+        app.init();
     }
 
     @AfterMethod
     public void tearDown() {
-        stop();
+        app.stop();
     }
 
 }
