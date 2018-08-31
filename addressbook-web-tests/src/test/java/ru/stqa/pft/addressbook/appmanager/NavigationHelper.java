a51b676258a6024f56eb284f81b15705ece4343f
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-08-31 13:07:20 +0300

    Создание нового базового класса Navigation Helper для ApplicationManager и замена наследования на дилегирование

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class NavigationHelper {
+    WebDriver wd;
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
