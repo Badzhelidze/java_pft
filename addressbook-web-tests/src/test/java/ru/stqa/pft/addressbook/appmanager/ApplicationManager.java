84fb463026b2fc7711e9cbc7c4ce61a5f0e473e2
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-09-07 15:45:00 +0300

    Добавление нового класса и замена "==" на "equals"

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -27,13 +27,13 @@ public class ApplicationManager {
 
 
     public void init() {
-        if (browser == BrowserType.CHROME) {
+        if (browser.equals(BrowserType.CHROME)) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.FIREFOX) {
+        } else if (browser.equals(BrowserType.FIREFOX)) {
             System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
             wd = new FirefoxDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             System.setProperty("webdriver.ie.driver", "IEDriverServer.exe");
             wd = new InternetExplorerDriver();
         }
