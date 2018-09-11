be5693e6113cec1b9769a52bac4692b64b421bba
Badzhelidze <36113378+Badzhelidze@users.noreply.github.com>
2018-09-11 14:01:01 +0300

    Создание счетчика групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -9,7 +10,12 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
 
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
+        //  List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        //   List<GroupData> after = app.getGroupHelper().getGroupList();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
 
     }
 
